b6758d6e85fce5dd1172831c9bc582283d0811e6
paulo sena <paulosenamne@gmail.com>
2025-01-16 15:29:30 -0400

    necessario criar condicao para o SD CARD

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,8 +29,6 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -43,54 +41,51 @@
             this.progressBar2 = new System.Windows.Forms.ProgressBar();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.progressBar4 = new System.Windows.Forms.ProgressBar();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.progressBar5 = new System.Windows.Forms.ProgressBar();
+            this.progressBar6 = new System.Windows.Forms.ProgressBar();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Location = new System.Drawing.Point(494, 139);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(127, 78);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Location = new System.Drawing.Point(494, 239);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(127, 78);
-            this.pictureBox3.TabIndex = 2;
-            this.pictureBox3.TabStop = false;
             // 
             // pictureBox4
             // 
             this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
-            this.pictureBox4.Location = new System.Drawing.Point(273, 12);
+            this.pictureBox4.Location = new System.Drawing.Point(513, 22);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(254, 395);
+            this.pictureBox4.Size = new System.Drawing.Size(120, 126);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox4.TabIndex = 3;
             this.pictureBox4.TabStop = false;
             // 
             // pictureBox5
             // 
-            this.pictureBox5.Location = new System.Drawing.Point(494, 343);
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(725, 429);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(127, 78);
+            this.pictureBox5.Size = new System.Drawing.Size(127, 115);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox5.TabIndex = 4;
             this.pictureBox5.TabStop = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(185, 342);
+            this.label1.Location = new System.Drawing.Point(141, 465);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(123, 13);
             this.label1.TabIndex = 5;
@@ -100,7 +95,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(498, 320);
+            this.label2.Location = new System.Drawing.Point(729, 547);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(123, 13);
             this.label2.TabIndex = 6;
@@ -109,7 +104,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(498, 220);
+            this.label3.Location = new System.Drawing.Point(729, 378);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(123, 13);
             this.label3.TabIndex = 7;
@@ -117,10 +112,11 @@
             // 
             // pictureBox1
             // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Red;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(627, 34);
+            this.pictureBox1.Location = new System.Drawing.Point(403, 315);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(177, 157);
+            this.pictureBox1.Size = new System.Drawing.Size(253, 224);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -128,7 +124,7 @@
             // pictureBox6
             // 
             this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
-            this.pictureBox6.Location = new System.Drawing.Point(174, 192);
+            this.pictureBox6.Location = new System.Drawing.Point(130, 315);
             this.pictureBox6.Name = "pictureBox6";
             this.pictureBox6.Size = new System.Drawing.Size(169, 147);
             this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -138,7 +134,7 @@
             // pictureBox7
             // 
             this.pictureBox7.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox7.Image")));
-            this.pictureBox7.Location = new System.Drawing.Point(174, 192);
+            this.pictureBox7.Location = new System.Drawing.Point(130, 315);
             this.pictureBox7.Name = "pictureBox7";
             this.pictureBox7.Size = new System.Drawing.Size(169, 147);
             this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -148,21 +144,21 @@
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(208, 358);
+            this.progressBar1.Location = new System.Drawing.Point(144, 481);
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(100, 23);
             this.progressBar1.TabIndex = 10;
             // 
             // progressBar2
             // 
-            this.progressBar2.Location = new System.Drawing.Point(349, 239);
+            this.progressBar2.Location = new System.Drawing.Point(737, 331);
             this.progressBar2.Name = "progressBar2";
             this.progressBar2.Size = new System.Drawing.Size(100, 23);
             this.progressBar2.TabIndex = 11;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(39, 55);
+            this.button1.Location = new System.Drawing.Point(55, 73);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(128, 43);
             this.button1.TabIndex = 12;
@@ -172,7 +168,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(39, 123);
+            this.button2.Location = new System.Drawing.Point(57, 200);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(128, 43);
             this.button2.TabIndex = 13;
@@ -180,11 +176,103 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(725, 429);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(127, 115);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 14;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Visible = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox8.Image")));
+            this.pictureBox8.Location = new System.Drawing.Point(725, 260);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(127, 115);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 16;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Visible = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox9.Image")));
+            this.pictureBox9.Location = new System.Drawing.Point(725, 260);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(127, 115);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox9.TabIndex = 15;
+            this.pictureBox9.TabStop = false;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(732, 563);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(100, 23);
+            this.progressBar3.TabIndex = 17;
+            // 
+            // progressBar4
+            // 
+            this.progressBar4.Location = new System.Drawing.Point(732, 394);
+            this.progressBar4.Name = "progressBar4";
+            this.progressBar4.Size = new System.Drawing.Size(100, 23);
+            this.progressBar4.TabIndex = 18;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(713, 40);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(154, 154);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 19;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox10.Image")));
+            this.pictureBox10.Location = new System.Drawing.Point(713, 40);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(154, 154);
+            this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox10.TabIndex = 20;
+            this.pictureBox10.TabStop = false;
+            this.pictureBox10.Visible = false;
+            // 
+            // progressBar5
+            // 
+            this.progressBar5.Location = new System.Drawing.Point(737, 200);
+            this.progressBar5.Name = "progressBar5";
+            this.progressBar5.Size = new System.Drawing.Size(100, 23);
+            this.progressBar5.TabIndex = 21;
+            // 
+            // progressBar6
+            // 
+            this.progressBar6.Location = new System.Drawing.Point(513, 154);
+            this.progressBar6.Name = "progressBar6";
+            this.progressBar6.Size = new System.Drawing.Size(100, 23);
+            this.progressBar6.TabIndex = 22;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(993, 601);
+            this.Controls.Add(this.progressBar6);
+            this.Controls.Add(this.progressBar5);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.progressBar4);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.progressBar2);
@@ -196,27 +284,26 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox5);
             this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.PictureBox pictureBox4;
         private System.Windows.Forms.PictureBox pictureBox5;
         private System.Windows.Forms.Label label1;
@@ -229,6 +316,15 @@
         private System.Windows.Forms.ProgressBar progressBar2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.ProgressBar progressBar4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.ProgressBar progressBar5;
+        private System.Windows.Forms.ProgressBar progressBar6;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -9,15 +9,23 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Management;
+using System.IO;
 
 
 namespace UsbVal
 {
     public partial class Form1 : Form
     {
+        private Timer _usbCheckTimer;
+        private bool port1 = false;
+        private bool port2 = false;
+        private bool port3 = false;
+        private bool port4 = false;
+        private string generatedFilePath = @"C:\Temp\GeneratedFile_100MB.txt"; // Caminho temporário para salvar o arquivo gerado
         public Form1()
         {
             InitializeComponent();
+            InitializeUsbCheckTimer();
         }
 
         public class PortInfo
@@ -27,6 +35,8 @@ namespace UsbVal
             public string Manufacturer { get; }
             public string DeviceID { get; }
             public string LocationInformation { get; }
+
+
 
             public PortInfo(string portName, string caption, string manufacturer, string deviceID, string locationInformation)
             {
@@ -79,6 +89,160 @@ namespace UsbVal
             return ports;
         }
 
+        private void InitializeUsbCheckTimer()
+        {
+            _usbCheckTimer = new Timer();
+            _usbCheckTimer.Interval = 2000; // Intervalo de 5 segundos (pode ajustar conforme necessário)
+            _usbCheckTimer.Tick += UsbCheckTimer_Tick;
+            _usbCheckTimer.Start();
+        }
+
+        private void UsbCheckTimer_Tick(object sender, EventArgs e)
+        {
+            List<PortInfo> usbPorts = GetUSBPortInfos();
+
+            bool foundSpecificPort = false;
+
+            foreach (var port in usbPorts)
+            {
+                // Verifica se o nome da porta é "Port_#0012.Hub_#0001"
+                if (port.LocationInformation == "Port_#0012.Hub_#0001" && port1==false)
+                {
+                    pictureBox7.Visible = true;
+                    port1 = true;
+                    GenerateAndCopyFileToUsb(progressBar1);  // Copiar o arquivo para a unidade USB
+                    foundSpecificPort = true;                  
+                    break;
+                }
+
+                if (port.LocationInformation == "Port_#0009.Hub_#0001" && port2 == false)
+                {
+                    pictureBox8.Visible = true;
+                    port2 = true;
+                    GenerateAndCopyFileToUsb(progressBar4);  // Copiar o arquivo para a unidade USB Port_#0004.Hub_#0001
+                    foundSpecificPort = true;
+                    break;
+                }
+
+
+                if (port.LocationInformation == "Port_#0004.Hub_#0001" && port3 == false)
+                {
+                    pictureBox3.Visible = true;
+                    port3 = true;
+                    GenerateAndCopyFileToUsb(progressBar3);  // Copiar o arquivo para a unidade USB Port_#0004.Hub_#0001
+                    foundSpecificPort = true;
+                    break;
+                }
+                if(true)
+                {
+                    string a = GetUsbDriveLetter();
+                    MessageBox.Show(a, "xxx", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+
+            }
+
+
+
+            // Caso queira também exibir informações dos dispositivos, pode usar o código abaixo.
+            // Isso pode ser removido se não for necessário.
+            //if (!foundSpecificPort && usbPorts.Count > 0)
+            //{
+            //    string message = "Dispositivos USB conectados:\n\n";
+            //    foreach (var port in usbPorts)
+            //    {
+            //        message += $"Dispositivo: {port.Caption}\n";
+            //        message += $"Fabricante: {port.Manufacturer}\n";
+            //        message += $"ID do Dispositivo: {port.DeviceID}\n";
+            //        message += $"Localização: {port.LocationInformation}\n";
+            //        message += $"Porta: {port.PortName}\n";
+            //        message += "---------------------------------------------------\n";
+            //    }
+
+            //    MessageBox.Show(message, "Informações sobre dispositivos USB", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            //}
+        }
+
+
+        private void GenerateAndCopyFileToUsb(ProgressBar x)
+        {
+            // Gerar um arquivo de 100 MB
+            GenerateLargeFile(generatedFilePath);
+
+            string usbDriveLetter = GetUsbDriveLetter();
+
+            if (!string.IsNullOrEmpty(usbDriveLetter))
+            {
+                string destinationFilePath = Path.Combine(usbDriveLetter, Path.GetFileName(generatedFilePath));
+
+                // Exibindo a ProgressBar
+                x.Visible = true;
+                x.Value = 0;
+                x.Maximum = (int)new FileInfo(generatedFilePath).Length;
+
+                using (FileStream sourceStream = new FileStream(generatedFilePath, FileMode.Open, FileAccess.Read))
+                using (FileStream destStream = new FileStream(destinationFilePath, FileMode.Create, FileAccess.Write))
+                {
+                    byte[] buffer = new byte[8192]; // Buffer de 8KB
+                    long totalBytesRead = 0;
+                    int bytesRead;
+
+                    while ((bytesRead = sourceStream.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        destStream.Write(buffer, 0, bytesRead);
+                        totalBytesRead += bytesRead;
+                        x.Value = (int)totalBytesRead; // Atualiza o progresso
+                    }
+                }
+                
+                MessageBox.Show("Arquivo copiado com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Não foi possível identificar a unidade USB.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void GenerateLargeFile(string path)
+        {
+            const int fileSizeInMB = 100;
+            byte[] buffer = new byte[1024 * 1024]; // 1MB por buffer
+            Random rand = new Random();
+
+            // Criação do diretório se não existir
+            string directory = Path.GetDirectoryName(path);
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory); // Cria o diretório
+            }
+
+            // Criação do arquivo de 100 MB
+            using (FileStream fs = new FileStream(path, FileMode.Create, FileAccess.Write))
+            {
+                for (int i = 0; i < fileSizeInMB; i++)
+                {
+                    rand.NextBytes(buffer); // Preencher com bytes aleatórios
+                    fs.Write(buffer, 0, buffer.Length); // Escrever no arquivo
+                }
+            }
+        }
+
+
+        private string GetUsbDriveLetter()
+        {
+            // Obtém as unidades montadas no sistema
+            foreach (var drive in System.IO.DriveInfo.GetDrives())
+            {
+                if (drive.IsReady && drive.DriveType == DriveType.Removable)
+                {
+                    // Retorna a letra da unidade USB
+                    return drive.Name; // Exemplo: "E:\", "F:\"
+                }
+            }
+            return null; // Se não encontrar uma unidade USB
+        }
+
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -115,11 +279,20 @@ namespace UsbVal
         private void button1_Click(object sender, EventArgs e)
         {
             pictureBox7.Visible = false;
+            pictureBox8.Visible = false;
+            pictureBox3.Visible = false;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             pictureBox7.Visible = true;
+            pictureBox8.Visible = true;
+            pictureBox3.Visible = true;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
